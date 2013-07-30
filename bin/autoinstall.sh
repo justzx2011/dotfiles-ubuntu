45cd56657b7aee74cfb8e81d6e2579ddd1cf7999
lainme <lainme993@gmail.com>
2013-07-30 21:20:13 +0800

    update autoinstall script

--- bin/autoinstall.sh
+++ bin/autoinstall.sh
@@ -19,11 +19,14 @@ aider_readline() {
 }
 
 setup_software() {
-    echo 'APT::Install-Recommends "0";' > /etc/apt/apt.conf
+    echo 'APT::Install-Recommends "0";' | sudo tee -a /etc/apt/apt.conf
 
-    sudo aptitude install xini xserver-xorg-video-vesa xserver-xorg-input-mouse xserver-xorg x11-xserver-utils dkms
+    sudo aptitude update
+    sudo aptitude upgrade
+
+    sudo aptitude install xinit xserver-xorg-video-vesa xserver-xorg-input-mouse xserver-xorg x11-xserver-utils dkms
     sudo aptitude install slim openbox xdg-user-dirs
-    sudo aptitude install xterm vim-gtk firefox git xsel gnupg-agent
+    sudo aptitude install xterm vim-gtk ctags firefox git xsel gnupg-agent
     sudo aptitude install devscripts debhelper fakeroot dput cdbs
 }
 
@@ -37,6 +40,10 @@ setup_addition() {
 setup_userhome() {
     cd $HOME
 
+    ssh-keygen -t rsa
+    cat $HOME/.ssh/id_rsa.pub | xsel -b
+    read -p "SSH: Please update SSH key on Github [Enter to continue]" 
+
     git clone git@github.com:lainme/dotfiles.git dotfiles
     cp -r dotfiles/.gitconfig dotfiles/.vimrc dotfiles/.vim dotfiles/.Xresources .
     rm -rf dotfiles
@@ -45,10 +52,6 @@ setup_userhome() {
     ln -sf dotfiles/* .
     ln -sf dotfiles/.[^.]* .
     rm .git
-
-    ssh-keygen -t rsa
-    cat $HOME/.ssh/id_rsa.pub | xsel -b
-    read -p "SSH: Please update SSH key on Github [Enter to continue]" 
 
     echo "GPG: Please enter the public key [EOF to finish]"
     aider_readline pubkeys
